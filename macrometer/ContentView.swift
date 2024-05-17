1fda007e6ff5370f397ecbfd156dca5ed24e3dec
Adam Campbell <adam.92.ac@gmail.com>
2024-05-18 09:31:34 +1000

    Inital project setup

---
+++ macrometer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macrometer
+//
+//  Created by Adam Campbell on 18/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macrometer/macrometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  macrometerApp.swift
+//  macrometer
+//
+//  Created by Adam Campbell on 18/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct macrometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
